0c3424ddb54f606bd9b75cabc1c994324cd24e2c
Adeniran John <adeniranjohn2016@gmail.com>
2023-06-25 08:57:46 +0100

    add school health centre

--- SchoolHealthCentre/urls.py
+++ SchoolHealthCentre/urls.py
@@ -1,22 +1,32 @@
-"""
-URL configuration for SchoolHealthCentre project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework_simplejwt import views as jwt_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="DLCF API",
+      default_version='v1',
+      description="Restful Implementation of School Health Centre API",
+      contact=openapi.Contact(email="adeniranjohn2016@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
+    re_path(
+        r"^$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="dlcf_api_docs",
+    ),
+        
     path("admin/", admin.site.urls),
+    path('api/v1/healthcentre/', include('HealthCentre.urls'))
 ]
